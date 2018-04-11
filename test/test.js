d3497de68bced4835cf5f2836d8cee4885302357
ferron <ferronrsmith@gmail.com>
2018-04-11 06:28:36 -0400

    quick update

--- test/test.js
+++ test/test.js
@@ -790,7 +790,6 @@ describe('ELASTICDUMP', function () {
   })
 
   describe('big int file to es', function () {
-
     it('works', function (done) {
       this.timeout(testTimeout)
       var options = {
